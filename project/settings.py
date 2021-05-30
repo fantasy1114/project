6b811c826293e481b3b6cbbfb6f2f85e6bfcf03b
Arterm <seniordeveloper00001@gmail.com>
2021-05-30 16:51:43 -0700

    fix

--- project/settings.py
+++ project/settings.py
@@ -129,7 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = BASE_DIR + '/static/'
 STATIC_URL = '/static/'
+
 
 AUTH_USER_MODEL = 'accounts.User'
 
