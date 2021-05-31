fb14b63ef3614eb43f467dab65e672338b1fe763
Arterm <seniordeveloper00001@gmail.com>
2021-05-30 21:04:25 -0700

    fix

--- accounts/viewsets.py
+++ accounts/viewsets.py
@@ -11,10 +11,7 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = UserSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-
-        print('***************')
         print(request.data)
-        print('***************')
         user.set_password(serializer.validated_data.get('password'))
         user.save()
         headers = self.get_success_headers(serializer.data)
