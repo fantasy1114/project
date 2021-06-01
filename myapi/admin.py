04220f0daae5443ffc56c94f83c56cc11c65c6e7
Arterm <seniordeveloper00001@gmail.com>
2021-06-01 11:10:34 -0700

    fix

--- myapi/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-from .models import Hero
-admin.site.register(Hero)
--- myapi/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import Hero
-
-class HeroSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Hero
-        fields = ('id', 'thumb', 'name', 'email', 'phone', 'designation')
